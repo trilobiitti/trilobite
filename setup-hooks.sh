8a46cddf6afc78de9ae5424f14ed8b6067fbb5ee
A.Bond <alexey.bond.94.55@gmail.com>
2020-12-17 21:06:46 +0600

    fix(pre-commit hook): Add changes preformed by ktlint to commit

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -3,8 +3,12 @@
 echo '#!/bin/sh
 
 cd "$GIT_DIR/.."
+FILES=$(git diff --cached --name-only --diff-filter=ACMR "*.kt" | sed "s| |\\ |g")
+[ -z "$FILES" ] && echo "Nothing to run ktlint on." && exit 0
 echo Running ktlint...
 ./gradlew :dna:ktlintFormat || exit 1
+echo "$FILES" | xargs git add
+exit 0
 ' > ./.git/hooks/pre-commit
 
 chmod +x ./.git/hooks/pre-commit
